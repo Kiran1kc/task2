a304c164e2949958499e311d3af1e11b66330e92
Kiran K C <137022813+Kiran1kc@users.noreply.github.com>
2024-01-15 18:37:26 +0545

    Add files via upload
    uploaded

---
+++ style.css.php
@@ -0,0 +1,132 @@
+ body {
+            font-family: 'Arial', sans-serif;
+            margin: 0;
+            padding: 0;
+            background-color: #f4f4f4;
+            color: #333;
+                    }
+
+        header {
+
+            background-color: skyblue;
+            color: #fff;
+            padding: 20px;
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+        }
+.navbar {
+  background-color: #333;
+  overflow: hidden;
+}
+
+.navbar ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+}
+
+.navbar li {
+  float: left;
+}
+
+.navbar a {
+  display: block;
+  color: white;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+}
+
+.navbar a:hover {
+  background-color: #ddd;
+  color: black;
+}
+
+        .left{
+            background-color: skyblue;
+            color: #fff;
+            padding: 20px;
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+/*            max-width: 250px; */
+
+        }
+
+.left img{
+    height: 120px;
+    border-radius: 25px;
+
+}
+.right h1 {
+      font-size: 1.5em;
+      margin-right: 20px;
+      float: left;
+    }
+.right img{
+    width: 100px;
+    display: block;
+    height: auto;
+}
+        .right {
+            max-width: 350px; /* Adjust the width as needed */
+      height: auto;
+      margin-right: 20px;
+      margin-right: 20px;
+            order: 2; 
+        }
+         section {
+      max-width: 1200px;
+      margin: 20px auto;
+      padding: 20px;
+      background-color: #fff;
+      box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+      border-radius: 5px;
+    }
+     #slider {
+            display: flex;
+            transition: transform 1.5s ease-in-out;
+        }
+
+        .slide {
+            min-width: 100%;
+            box-sizing: border-box;
+        }
+
+
+
+.pk img{
+width: 1450px;
+height: 400px;
+position: relative;
+overflow: hidden;
+
+  }
+    p {
+      line-height: 1.6;
+    }
+
+    .cta-button {
+      display: inline-block;
+      padding: 10px 20px;
+      background-color: #007BFF;
+      color: #fff;
+      text-decoration: none;
+      font-weight: bold;
+      border-radius: 5px;
+      transition: background-color 0.3s ease;
+    }
+
+    .cta-button:hover {
+      background-color: #0056b3;
+    }
+
+
+    footer {
+      text-align: center;
+      padding: 20px;
+      background-color: #333;
+      color: #fff;
+    }
